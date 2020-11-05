d27168d7e61dd4d7e324d98c939eba88eef6b00d
Eli Thorpe <ethds24@gmail.com>
2020-11-05 13:00:02 -0500

    clean up buzzer, add robot config

--- pikitlib/robot.py
+++ pikitlib/robot.py
@@ -5,21 +5,17 @@ from networktables import NetworkTables
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+import robotmap
 
-#MOTOR CHANNEL
-FRONT_LEFT = 1
-BACK_LEFT = 2
-BACK_RIGHT = 3
-FRONT_RIGHT = 4
 
 class MyRobot():
     def robotInit(self):
         """Robot initialization function"""
         # object that handles basic drive operations
-        self.leftBackMotor = pikitlib.SpeedController(BACK_LEFT)
-        self.leftFrontMotor = pikitlib.SpeedController(FRONT_LEFT)
-        self.rightBackMotor = pikitlib.SpeedController(BACK_RIGHT)
-        self.rightFrontMotor = pikitlib.SpeedController(FRONT_RIGHT)
+        self.leftBackMotor = pikitlib.SpeedController(robotmap.BACK_LEFT)
+        self.leftFrontMotor = pikitlib.SpeedController(robotmap.FRONT_LEFT)
+        self.rightBackMotor = pikitlib.SpeedController(robotmap.BACK_RIGHT)
+        self.rightFrontMotor = pikitlib.SpeedController(robotmap.FRONT_RIGHT)
 
         self.left = pikitlib.SpeedControllerGroup(self.leftBackMotor, self.leftFrontMotor)
         self.right = pikitlib.SpeedControllerGroup(self.rightBackMotor, self.rightFrontMotor )
@@ -72,7 +68,7 @@ class MyRobot():
         # Test controller
         
         forward = self.driver.getX(0)
-        forward = 0.80 * self.deadzone(forward, 0.2)
+        forward = 0.80 * self.deadzone(forward, robotmap.DEADZONE)
         rotation_value = -0.8 * self.driver.getY(1)
         self.myRobot.arcadeDrive(forward,rotation_value)
 
