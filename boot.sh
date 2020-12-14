c13bbfa8cd5a1b49797451879218c61820012c69
Austin <austinjvetor@gmail.com>
2020-12-13 19:32:58 -0500

    instructions for autostart

--- boot.sh
+++
@@ -1,6 +0,0 @@
-if [[ $(pidof python run.py) ]];
-then
-    echo "run.py is already streaming!";
-else
-    echo "run.py is *NOT YET* running, so we'll start it up!";
-    cd /home/pi/RobotKitLib/pikitlib/run.py
